393a846458d3ca2721870b156d6d710a7ad9c6ee
John Towne <towne.john@gmail.com>
2026-09-11 21:06:59 -0700

    Added refs module

---
+++ src/refs.py
@@ -0,0 +1,13 @@
+"""Bibliographic information and references."""
+
+dippr = "Rowley, R. L.; Wilding, W. V.; Oscarson, J. L.; Knotts, T. A.; Giles, N. F. DIPPR Data Compilation of Pure " \
+        "Chemical Properties; Design Institute for Physical Properties, AIChE: New York, NY, 2016."
+
+yaws = "Yaws, C. L. Thermophysical properties of chemicals and hydrocarbons, 2nd ed., Gulf Professional Publishing: " \
+       "Waltham, MA, 2014."
+
+perry = "Green, D. W.; Southard, M. Z. Perry's Chemical Engineers' handbook; McGraw-Hill Education: New York, NY, 2019."
+
+pr = "Peng, D. Y.; Robinson, D. B. A New Two-Constant Equation of State. Ind. Eng. Chem. Fundamen. 1976, 15, 59–64."
+
+
